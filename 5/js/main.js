084eb30a6182e3108ac27343054fa1665dde9943
Keks <keks@htmlacademy.ru>
2025-04-22 08:32:59 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,8 @@
+import { generatePhotosData } from './data.js';
+
+const photosData = generatePhotosData();
+console.log(photosData);
+
+import { renderThumbnails } from './thumbnails.js';
+
+renderThumbnails();
