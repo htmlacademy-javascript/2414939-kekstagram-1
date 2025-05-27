a9e66035558e0d0549d0d7c73546d9e57a677c3e
Иван <orient@211.ru>
2025-05-27 15:03:54 +0700

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { generatePhotosData } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture.js';
+import './forms.js';
 
 
 const photosData = generatePhotosData();
