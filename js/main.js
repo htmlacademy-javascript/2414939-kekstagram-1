76510fc556ed033c834e6200bb865349de6f29ed
Иван <orient@211.ru>
2025-06-04 12:23:00 +0700

    Правда или действие

--- js/main.js
+++ js/main.js
@@ -1,48 +1,9 @@
 import { generatePhotosData } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture.js';
-import './form.js';
+import './forms.js';
 
-// import { showOverlay, hideOverlay } from './form.js';
-import { initScaleHandlers } from './scale.js';
-import { initSlider, applyEffect } from './slider.js';
-import { isEscape } from './utils.js';
 
-// Генерация данных фотографий
 const photosData = generatePhotosData();
 
-// Отрисовка миниатюр с возможностью открытия большой картинки
 renderThumbnails(photosData, openBigPicture);
-
-// Обработчик открытия окна при выборе файла
-const fileChooser = document.querySelector('#upload-file');
-
-fileChooser.addEventListener('change', () => {
-  showOverlay();
-});
-
-// Обработчик закрытия окна по кнопке отмены
-const cancelButton = document.querySelector('#upload-cancel');
-cancelButton.addEventListener('click', () => hideOverlay());
-
-// Инициализация масштабирования
-initScaleHandlers();
-
-// Инициализация слайдера эффектов
-initSlider();
-
-// Назначение обработчика изменения эффекта
-document.querySelectorAll('.effects__radio').forEach((radio) => {
-  radio.addEventListener('change', () => applyEffect(radio.value));
-});
-
-// Инициализация формы
-import { initForm } from './form.js';
-initForm();
-
-// Обработка нажатия Escape для закрытия окна
-document.addEventListener('keydown', (e) => {
-  if (isEscape(e)) {
-    hideOverlay();
-  }
-});
