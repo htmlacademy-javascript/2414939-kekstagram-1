de297633a5a009da5961a295b2abe731cf04de39
Keks <keks@htmlacademy.ru>
2025-06-04 11:43:00 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/scale.js
+++ 8/js/scale.js
@@ -1,38 +1,44 @@
 // scale.js
 import { isEscape } from './utils.js';
 
-// Глобальные переменные
+// Объект состояния масштаба
+export const scaleState = {
+  currentScale: 100,
+};
+
+// DOM элементы
 const scaleValue = document.querySelector('.scale__control--value');
 const previewImage = document.querySelector('.img-upload__preview > img');
 const smallerBtn = document.querySelector('.scale__control--smaller');
 const biggerBtn = document.querySelector('.scale__control--bigger');
 
-let currentScale = 100;
+// Функция обновления отображения масштаба
+function updateScale() {
+  scaleValue.value = `${scaleState.currentScale}%`;
+  previewImage.style.transform = `scale(${scaleState.currentScale / 100})`;
+}
 
 // Функция увеличения масштаба
 function increaseScale() {
-  if (currentScale < 100) {
-    currentScale += 25;
+  if (scaleState.currentScale < 100) {
+    scaleState.currentScale += 25;
     updateScale();
   }
 }
 
 // Функция уменьшения масштаба
 function decreaseScale() {
-  if (currentScale > 25) {
-    currentScale -= 25;
+  if (scaleState.currentScale > 25) {
+    scaleState.currentScale -= 25;
     updateScale();
   }
 }
 
-// Обновляет отображаемый масштаб
-function updateScale() {
-  scaleValue.value = `${currentScale}%`;
-  previewImage.style.transform = `scale(${currentScale / 100})`;
-}
-
-// Регистрируем обработчики для кнопок масштабирования
+// Регистрация обработчиков кнопок
 smallerBtn.addEventListener('click', decreaseScale);
 biggerBtn.addEventListener('click', increaseScale);
 
-export { increaseScale, decreaseScale, updateScale };
+// Инициализация начального отображения
+updateScale();
+
+export { increaseScale, decreaseScale, updateScale, scaleState };
