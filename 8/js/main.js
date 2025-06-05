ffa42caf7e4d36771e1add3cf7d1d706ed01c182
Keks <keks@htmlacademy.ru>
2025-06-05 16:33:12 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/main.js
+++ 8/js/main.js
@@ -6,6 +6,11 @@ import { increaseScale, decreaseScale, updateScale } from './scale.js';
 import { applyEffect, removeAllEffectClasses, updateEffectStyle } from './slider.js';
 import { onFileSelected, showOverlay, hideOverlay, sendForm, resetForm } from './forms.js';
 
+const photosData = generatePhotosData();
+
+renderThumbnails(photosData, openBigPicture);
+
+
 // Дождаться полной загрузки DOM
 document.addEventListener('DOMContentLoaded', () => {
 });
