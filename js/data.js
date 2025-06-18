9022d2b5fd66c1c98d92f3871277f4d83af78b09
Иван <orient@211.ru>
2025-06-18 13:51:32 +0700

    Надо подкачаться

--- js/data.js
+++ js/data.js
@@ -1,3 +1,20 @@
+export function generatePhotosData() {
+  const photos = [];
+
+  for (let i = 1; i <= 25; i++) {
+    const photo = {
+      id: i,
+      url: `photos/${i}.jpg`,
+      description: getRandomArrayElement(descriptions),
+      likes: getRandomInt(15, 200),
+      comments: generateComments(getRandomInt(0, 5))
+    };
+
+    photos.push(photo);
+  }
+
+  return photos;
+}
 
 export async function loadPhotosFromServer(url) {
   try {
