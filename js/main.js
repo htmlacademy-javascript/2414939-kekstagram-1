2c903e317151fe68972b0e78cb4f01693e824442
Иван <orient@211.ru>
2025-06-24 00:34:30 +0700

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// main3.js
+// main2.js
 
 import { loadPhotosFromServer } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
@@ -6,7 +6,7 @@ import { openBigPicture } from './big-picture.js';
 import { increaseScale, decreaseScale, updateScale } from './scale.js';
 import { applyEffect, removeAllEffectClasses, updateEffectStyle } from './slider.js';
 import { onFileSelected, showOverlay, hideOverlay, sendForm, resetForm } from './forms.js';
-import { sendDataToServer, blockSubmitButton, unblockSubmitButton, createFormData, showSuccessMessage, showErrorMessage } from './server.js';
+import { sendDataToServer, blockSubmitButton, unblockSubmitButton, createFormData } from './server.js';
 
 const DATA_URL = 'https://28.javascript.htmlacademy.pro/kekstagram/data';
 
