ec070958d9285b964df27cc440c97d6aa677fb92
Keks <keks@htmlacademy.ru>
2025-06-24 03:16:33 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/server.js
+++ 10/js/server.js
@@ -1,4 +1,4 @@
-// server7.js
+// server1.js
 
 import { isEscape } from './utils.js';
 
@@ -36,7 +36,7 @@ export function showSuccessMessage() {
   body.appendChild(successMessage);
 
   document.addEventListener('click', (e) => {
-    if (e.target.matches('.success__button') || e.target.closest('.success') === null) {
+    if (e.target.classList.contains('success__button')) { // Ждём точное попадание на кнопку
       removeMessage(successMessage);
     }
   });
@@ -54,7 +54,7 @@ export function showErrorMessage() {
   body.appendChild(errorMessage);
 
   document.addEventListener('click', (e) => {
-    if (e.target.matches('.error__button') || e.target.closest('.error') === null) {
+    if (e.target.classList.contains('error__button')) { // Аналогичное условие для ошибки
       removeMessage(errorMessage);
     }
   });
