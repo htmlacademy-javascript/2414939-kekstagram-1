f768bbcc8f64fb17da7b04e3ee94f732139c610d
Иван <orient@211.ru>
2025-06-25 15:23:54 +0700

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture.js';
-import { hideOverlay, resetForm, blockSubmitButton, unblockSubmitButton, showOverlay} from './forms.js';
-import { sendDataToServer, createFormData,loadPhotosFromServer} from './server.js';
+import { hideOverlay, resetForm, blockSubmitButton, unblockSubmitButton, showOverlay } from './forms.js';
+import { sendDataToServer, createFormData, loadPhotosFromServer, DATA_URL } from './server.js';
 
 // const DATA_URL = 'https://28.javascript.htmlacademy.pro/kekstagram/data';
 
