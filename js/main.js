0e92d998fcfa3ddc62ee34856134bab7112170b0
Иван <orient@211.ru>
2025-06-25 12:18:22 +0700

    Надо подкачаться

--- js/main.js
+++ js/main.js
@@ -20,21 +20,21 @@ const DATA_URL = 'https://28.javascript.htmlacademy.pro/kekstagram/data';
   function initComponents() {}
 })();
 
-// Функция для отправки формы
-async function handleFormSubmit(formData) {
-  const submitButton = document.querySelector('button[type="submit"]');
-  blockSubmitButton(submitButton);
+// // Функция для отправки формы
+// async function handleFormSubmit(formData) {
+//   const submitButton = document.querySelector('button[type="submit"]');
+//   blockSubmitButton(submitButton);
 
-  const isSuccess = await sendDataToServer(formData);
+//   const isSuccess = await sendDataToServer(formData);
 
-  if (isSuccess) {
-    resetForm();
-    hideOverlay();
-  }
-  // else {}
+//   if (isSuccess) {
+//     resetForm();
+//     hideOverlay();
+//   }
+//   // else {}
 
-  unblockSubmitButton(submitButton);
-}
+//   unblockSubmitButton(submitButton);
+// }
 
 // Регистрирует обработчик отправки формы
 const uploadForm = document.querySelector('#upload-select-image');
