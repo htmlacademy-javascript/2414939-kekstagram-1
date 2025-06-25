4f7c92c4da483888015eb0aed885b202db265800
Иван <orient@211.ru>
2025-06-25 10:28:27 +0700

    Надо подкачаться

--- js/data.js
+++ js/data.js
@@ -16,15 +16,15 @@ export function generatePhotosData() {
   return photos;
 }
 
-export async function loadPhotosFromServer(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Ошибка загрузки данных (${response.status})`);
-    }
-    return await response.json();
-  } catch (error) {
-    // console.error('Ошибка загрузки данных:', error.message);
-    return [];
-  }
-}
+// export async function loadPhotosFromServer(url) {
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Ошибка загрузки данных (${response.status})`);
+//     }
+//     return await response.json();
+//   } catch (error) {
+//     // console.error('Ошибка загрузки данных:', error.message);
+//     return [];
+//   }
+// }
