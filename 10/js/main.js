524110d1ea39847b0517299c00f8699088cf378a
Keks <keks@htmlacademy.ru>
2025-06-25 05:30:25 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/main.js
+++ 10/js/main.js
@@ -1,9 +1,9 @@
 import { loadPhotosFromServer } from './data.js';
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture.js';
-import { increaseScale, decreaseScale, updateScale } from './scale.js';
-import { applyEffect, removeAllEffectClasses, updateEffectStyle } from './slider.js';
-import { onFileSelected, showOverlay, hideOverlay, sendForm, resetForm } from './forms.js';
+// import { increaseScale, decreaseScale, updateScale } from './scale.js';
+// import { applyEffect, removeAllEffectClasses, updateEffectStyle } from './slider.js';
+import { hideOverlay, resetForm } from './forms.js';
 import { sendDataToServer, blockSubmitButton, unblockSubmitButton, createFormData } from './server.js';
 
 const DATA_URL = 'https://28.javascript.htmlacademy.pro/kekstagram/data';
