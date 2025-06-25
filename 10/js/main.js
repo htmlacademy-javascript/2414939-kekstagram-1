1d0d181f50d865572cda65ad7e1e4657f426f095
Keks <keks@htmlacademy.ru>
2025-06-25 11:36:45 +0300

    :heavy_check_mark: Сборка #10

--- 10/js/main.js
+++ 10/js/main.js
@@ -1,23 +1,25 @@
 
 import { renderThumbnails } from './thumbnails.js';
 import { openBigPicture } from './big-picture.js';
-import { hideOverlay, resetForm, blockSubmitButton, unblockSubmitButton, showOverlay } from './forms.js';
-import { sendDataToServer, createFormData, loadPhotosFromServer, DATA_URL } from './server.js';
+// import { hideOverlay, resetForm, blockSubmitButton, unblockSubmitButton, showOverlay } from './forms.js';
+import { loadPhotosFromServer, DATA_URL } from './server.js';
+import './forms.js';
 
 // const DATA_URL = 'https://28.javascript.htmlacademy.pro/kekstagram/data';
 
 // Основной асинхронный блок для загрузки данных с сервера2
-(async () => {
-  try {
-    const photosData = await loadPhotosFromServer(DATA_URL);
-    if (photosData) {
-      renderThumbnails(photosData, openBigPicture);
-    }
-    // else {}
-  } catch (error) {}
+// (async () => {
+//   try {
+//     const photosData = await loadPhotosFromServer(DATA_URL);
+//     if (photosData) {
+//       renderThumbnails(photosData, openBigPicture);
+//     }
+//     // else {}
+//   } catch (error) {}
 
-  initComponents();
+//   initComponents();
 
-  function initComponents() {}
-})();
-
+//   function initComponents() {}
+// })();
+const photos = await loadPhotosFromServer();
+renderThumbnails(photos, openBigPicture);
