a53b87d287ebfbc6376adfc75caaf14e4d04b13d
Keks <keks@htmlacademy.ru>
2025-07-07 06:51:41 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/filter.js
+++ 11/js/filter.js
@@ -1,74 +1,56 @@
+// // Экспортируем необходимые функции
 // import { renderThumbnails } from './thumbnails.js';
-// import { openBigPicture } from './big-picture.js';
-// import { loadPhotosFromServer } from './server.js';
-// import './forms.js';
-
-// // Находим контейнер фильтров и показываем его после загрузки изображений
-// const filtersContainer = document.querySelector('.img-filters');
-// filtersContainer.classList.remove('img-filters--inactive');
-
-// let currentPhotos = []; // Хранит загруженные фотографии
-
-// // Загрузка фотографий с сервера
-// loadPhotosFromServer()
-//   .then((photos) => {
-//     currentPhotos = photos;
-
-//     // Отрисовка начальных фотографий
-//     renderThumbnails(currentPhotos, openBigPicture);
-
-//     // Настройка обработчиков фильтров
-//     setupFilters();
-//   })
-//   .catch(error => console.error('Ошибка загрузки:', error));
-
-// function setupFilters() {
+// export const setupFilters = (photos) => {
+//   // Получаем элементы фильтров
 //   const defaultBtn = document.getElementById('filter-default');
 //   const randomBtn = document.getElementById('filter-random');
 //   const discussedBtn = document.getElementById('filter-discussed');
 
-//   // Объявляем обработчики для каждого фильтра
+//   // Сохраняем текущие фотографии
+//   let currentPhotos = photos;
+
+//   // Обработчики для каждого фильтра
 //   function handleDefaultClick() {
-//     renderThumbnails(currentPhotos, openBigPicture); // Показываем первоначальную коллекцию
+//     renderThumbnails(currentPhotos, openBigPicture);
 //     setActiveFilter(defaultBtn);
 //   }
 
 //   function handleRandomClick() {
-//     const shuffled = shuffleArray([...currentPhotos]); // Перемешиваем случайным образом
+//     const shuffled = shuffleArray([...currentPhotos]);
 //     renderThumbnails(shuffled, openBigPicture);
 //     setActiveFilter(randomBtn);
 //   }
 
 //   function handleDiscussedClick() {
-//     const sorted = sortByComments([...currentPhotos]); // Сортируем по количеству комментариев
+//     const sorted = sortByComments([...currentPhotos]);
 //     renderThumbnails(sorted, openBigPicture);
 //     setActiveFilter(discussedBtn);
 //   }
 
-//   // Обрабатываем событие click для всех кнопок
+//   // Добавляем обработчики событий
 //   defaultBtn.addEventListener('click', handleDefaultClick);
 //   randomBtn.addEventListener('click', handleRandomClick);
 //   discussedBtn.addEventListener('click', handleDiscussedClick);
-// }
+// };
 
 // // Установка активного фильтра
-// function setActiveFilter(button) {
+// export const setActiveFilter = (button) => {
 //   document.querySelectorAll('.img-filters__button').forEach(btn => btn.classList.remove('img-filters__button--active'));
 //   button.classList.add('img-filters__button--active');
-// }
+// };
 
 // // Вспомогательные функции
-// function shuffleArray(array) {
+// export const shuffleArray = (array) => {
 //   for (let i = array.length - 1; i > 0; i--) {
-//     const j = Math.floor(Math.random() * (i + 1)); // Случайная позиция
+//     const j = Math.floor(Math.random() * (i + 1));
 //     [array[i], array[j]] = [array[j], array[i]];
 //   }
 //   return array;
-// }
+// };
 
-// function sortByComments(photos) {
+// export const sortByComments = (photos) => {
 //   return photos.sort((a, b) => b.comments.length - a.comments.length);
-// }
+// };
 
 
 import { renderThumbnails } from './thumbnails.js';
